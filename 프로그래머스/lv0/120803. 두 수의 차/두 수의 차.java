e398a3e625e758648b6a7349b5a56880e51d3eeb
yoohwanihn <73772238+yoohwanihn@users.noreply.github.com>
2023-04-26 16:47:48 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+    }
+    public static void main(String args[]){
+        int print = solution(20,30);
+        System.out.println(print);
+    }
+}
