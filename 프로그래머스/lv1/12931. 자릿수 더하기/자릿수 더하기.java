e3c604d09e0460160150819ba45d117d5a7da9a6
yoohwanihn <73772238+yoohwanihn@users.noreply.github.com>
2023-05-01 16:24:03 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.14 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(n!=0){
+            answer = answer + n%10;
+            n=n/10;
+        }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println(answer);
+        return answer;
+    }
+}
