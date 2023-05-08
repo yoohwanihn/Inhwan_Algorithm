7281a13010640bebc927a7fcaeca74ca1fc990d9
치타유환인 <73772238+yoohwanihn@users.noreply.github.com>
2023-05-08 11:36:15 +0900

    [level 0] Title: 가위 바위 보, Time: 3.07 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        for(String str : rsp.split("")){
+            if(str.equals("2")){
+                answer+="0";
+            }
+            else if(str.equals("0")){
+                answer+="5";
+            }
+            else answer+= "2";
+        }
+        return answer;
+    }
+}
