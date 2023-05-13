b21a7787abde4f4692c353211f92093be200362e
치타유환인 <73772238+yoohwanihn@users.noreply.github.com>
2023-05-13 17:57:10 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 1.21 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -3,7 +3,7 @@ class Solution {
         String answer = my_string.substring(0,s);
         answer = answer + overwrite_string;
         if(my_string.length() > overwrite_string.length() + s){
-            answer += my_string.substring(overwrite_string.length()+s,my_string.length());
+            answer += my_string.substring(overwrite_string.length()+s);
         }
         return answer;
     }
