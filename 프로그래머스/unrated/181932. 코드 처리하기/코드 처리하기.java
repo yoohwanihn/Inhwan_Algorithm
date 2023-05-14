ab6deb18302618be5d995cdc943f0095eb412d6c
치타유환인 <73772238+yoohwanihn@users.noreply.github.com>
2023-05-14 17:10:55 +0900

    [unrated] Title: 코드 처리하기, Time: 1.69 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -13,10 +13,7 @@ class Solution {
                     continue;
                 }
             }
-            if(mode==0 && i%2==0){
-                answer+=code.charAt(i);
-            }
-            else if(mode==1 && i%2==1){
+            if(i%2==mode){
                 answer+=code.charAt(i);
             }
         }
