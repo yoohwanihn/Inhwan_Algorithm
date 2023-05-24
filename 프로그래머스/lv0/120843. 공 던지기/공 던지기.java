672c85e64743063a7b3b845c5ee4c06f60bf9109
치타유환인 <73772238+yoohwanihn@users.noreply.github.com>
2023-05-24 10:26:29 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        int index = 0;
+        for(int i=1;i<k;i++){
+            index += 2;
+        }
+        index = index % numbers.length;
+        answer = numbers[index];
+        return answer;
+    }
+}
