21e108a757693441a1180f336bdf1b60407a238a
치타유환인 <73772238+yoohwanihn@users.noreply.github.com>
2023-05-30 14:02:40 +0900

    [unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,12 +1,15 @@
 class Solution {
     public String solution(String n_str) {
         String answer = "";
+        int idx = -1;
         for(int i=0;i<n_str.length();i++){
-            if(n_str.charAt(i)=='0'&&answer.equals("")){
+            if(n_str.charAt(i)=='0'){
                 continue;
             }
-            answer+=n_str.charAt(i);
+            idx=i;
+            break;
         }
+        answer = n_str.substring(idx);
         return answer;
     }
 }
