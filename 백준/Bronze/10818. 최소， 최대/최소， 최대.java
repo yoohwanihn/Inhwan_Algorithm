6b45786abdd8023a752ee4f95bcce4482e1b41a5
치타유환인 <73772238+yoohwanihn@users.noreply.github.com>
2023-06-01 23:55:32 +0900

    [Bronze III] Title: 최소, 최대, Time: 1768 ms, Memory: 311112 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            int num = sc.nextInt();
+            if(num>max){
+                max = num;
+            }
+            if(num<min){
+                min = num;
+            }
+        }
+        System.out.print(min + " " + max);
+    }
+}
