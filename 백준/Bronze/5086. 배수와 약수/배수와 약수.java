c9060ed705be9d5792ab9071fa3659f29e8fd8b9
치타유환인 <73772238+yoohwanihn@users.noreply.github.com>
2023-06-02 19:11:50 +0900

    [Bronze III] Title: 배수와 약수, Time: 204 ms, Memory: 17460 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		while(true){
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			if(B==0) break;
+			
+			if(B%A==0) {
+			System.out.println("factor");
+			}
+			else if(A%B==0) {
+			System.out.println("multiple");
+			}
+			else {
+				System.out.println("neither");
+			}
+		}
+	}
+}
