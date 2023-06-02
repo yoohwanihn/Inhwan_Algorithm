bb24dea68ffeb8dc11865dcf85eae6710306b03c
치타유환인 <73772238+yoohwanihn@users.noreply.github.com>
2023-06-02 11:16:56 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 216 ms, Memory: 17900 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        BigInteger n = sc.nextBigInteger();
+        BigInteger m = sc.nextBigInteger();
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+    }
+}
