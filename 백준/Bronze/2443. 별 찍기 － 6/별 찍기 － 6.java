7b4abf62ce5a5688376fec32c6a3117b2ce7f393
치타유환인 <73772238+yoohwanihn@users.noreply.github.com>
2023-06-06 22:33:34 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 396 ms, Memory: 19516 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				if(j<i) {
+					System.out.print(" ");
+				}
+				else {
+					System.out.print("*");
+				}
+			}
+			for(int j=N-1;j>0;j--) {
+				if(j>i) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
