d49c23b944743e56a0f03c427e51b02eef4fd3a1
치타유환인 <73772238+yoohwanihn@users.noreply.github.com>
2023-08-22 18:29:51 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 144 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<N;i++) {
+        	String[] str = br.readLine().split(" ");
+        	sb.append("Case #" + (i+1) + ": ");
+        	for(int j=str.length-1;j>=0;j--) {
+        		sb.append(str[j]+ " ");
+        	}
+        	sb.append("\n");
+        }
+        System.out.print(sb.toString());
+	}
+}
